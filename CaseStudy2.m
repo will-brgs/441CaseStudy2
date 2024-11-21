14bdb00737952187b69bbbed5e2fe856fd3bac35
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-11-21 13:44:08 -0600

    yippee!!!!

---
+++ CaseStudy2.m
@@ -0,0 +1,12 @@
+%% ESE441 Case Study 2: 
+%% Introduction
+% * Authors:                  Will Burgess, Mack LaRosa
+% * Class:                    ESE 441
+% * Date:                     Created 11/21/2024, Last Edited 12/09/2024
+%% Housekeeping
+close all
+clear
+clc
+code = "finished";
+%%
+
