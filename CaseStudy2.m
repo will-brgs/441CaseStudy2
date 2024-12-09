d40d88f7915a51207b77a8a3ffdf978f4ae9c9f3
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-12-08 22:16:05 -0600

    sdfghjk

--- CaseStudy2.m
+++ CaseStudy2.m
@@ -29,7 +29,7 @@ t = 0:dt:tLim;
 
 IC = [Gb, 0, Ib]; %should baseline insulin action be zero???
 
-%%
+%% Deterine Observer/Controller
 A = [-p1, -Gb, 0;
      0, -p2, p3;
      0, 0, -n];
@@ -39,39 +39,18 @@ B = [0;
 C = [1, 0, 0];
 
 %[K_p, K_i, L] = findGains(A, B, C, [-5.3, -5.2, -5.1, -3], [-1, -1.1, -1.2]);
-[K_p, K_i, L] = findGains(A, B, C, [-5.0, -5.1, -5.2, -5.3], [-6, -6.2, -6.4]);
-
-
-% Aa = [-p1, -Gb, 0, 0;
-%       0, -p2, p3, 0;
-%       0, 0, -n, 0;
-%       1, 0, 0, 0];
-% Ba = [0; 
-%       0; 
-%       1; 
-%       0];
-% Br = [0; 0 ; 0; -1];
-% Ca = [1, 0, 0, 0];
-% Da = [0];
-% 
-% p4 = -300;
-% Ka = place(Aa,Ba,[p1,p2,p3,p4]);
-% 
-% K_p = Ka(1:3);
-% k_i = Ka(4);
-% 
+[Kp, Ki, L] = findGains(A, B, C, [-5.0, -5.1, -5.2, -5.3], [-6, -6.2, -6.4]);
 
 AAugmented = [A, zeros(3,1); 
-                  C, 0];
+              C, 0];
 BAugmented = [B; 0];
 
 kAugmented = place(A, B, [-5.3, -5.2, -5.1]);
-K_p = kAugmented(1:3);
-K_i = kAugmented(end);
+Kp = kAugmented(1:3);
+Ki = kAugmented(end);
 
-
-%%
-for j = 3 % Varies mode from auto to exercise. change to 2 later
+%% Define Mode
+for j = 1 % Varies mode from auto to exercise. change to 2 later
     if j == 1
         mode = 'Auto';
     elseif j==2
@@ -80,6 +59,7 @@ for j = 3 % Varies mode from auto to exercise. change to 2 later
         mode = 'Uncontrolled';
     end
 for i = 2 % Sweeps through D values, change to 4 later
+%% Define Disturbance
 if i == 1
 D = DGenerate('Monophasic',t,20,0,70,20,50, 0);
 disturbance = 'Monophasic Eating';
@@ -113,79 +93,17 @@ end
 grid on;
 end
     
-%%
+%% Solve ODE with Control
 Dinterp = @(t) interp1(0:dt:tLim, D, t, 'linear', 'extrap');
 
-% dynamics = @(t, y) [
-%             -p1 * (y(1) - Gb) - y(2) * y(1);
-%             -p2 * y(2) + p3 * (y(3) - Ib);
-%             -n * y(3) ];
-
-dynamics = @(t, y, u) [
-    -p1 * (y(1) - Gb) - y(2) * y(1) + Dinterp(t);
-     -p2 * y(2) + p3 * (y(3) - Ib);
-    -n * y(3) + u];
-
-% Pre-allocate storage for the states (G, X, I)
-nonLinear = zeros(length(t), 3); % 3 columns for G(t), X(t), and I(t)
-nonLinear(1, :) = IC;            % Set initial condition
-
-u =  zeros(length(t), 1);
-u(1) = 0; %initial u
-
-vHat = IC;
-% Manual simulation loop
-for h = 1:(length(t)-1)
-    % Current state
-    vCurrent = nonLinear(h, :); % Row vector [G, X, I] at time step h
-    
-    vHatDot = A * vHat(h, :)' + B * u(h) + L * (vCurrent(1) - vHat(h, 1));
-    vHat(h+1, :) = (vHat(h, :)' + vHatDot * dt)'; % Transpose back to row vector
-    vHat(h+1, :) = max(vHat(h+1, :), 0); % ensure nonnegativity
-
-
-    z = (Gb * ones(h,1)) - nonLinear(1:h,1);
-    z = sum(z);
-
-    %u(h+1) = K_p * vHat(h, :)' + K_i * z;
-
-    u(h+1) = -K_p * vHat(h, :)';
-    % if u(h+1, 1) <= 0
-    %     u(h+1, 1) = 0;    
-    % end
- 
-    dvdt = dynamics(t(h), vCurrent', u(h)); % Pass the current state as a column vector
-    
-    nonLinear(h+1, :) = vCurrent + dvdt' * dt; % Transpose dydt back to row vector
-    
-    if vHat(h+1, 1) <= 0
-        vHat(h+1, 1) = 0;    
-    end
-    if vHat(h+1, 2) <= 0
-        vHat(h+1, 2) = 0;    
-    end
-    if vHat(h+1, 3) <= 0
-        vHat(h+1, 3) = 0;    
-    end
-
-    if all(nonLinear(h+1, 1) <= 0)
-        nonLinear(h+1, 1) = 0;    
-    end
-    if all(nonLinear(h+1, 2) <= 0)
-        nonLinear(h+1, 2) = 0;    
-    end
-    if all(nonLinear(h+1, 3) <= 0)
-        nonLinear(h+1, 3) = 0;    
-    end
-    
-    % vHat(h+1, :) = max(vHat(h+1, :), 0); %ensure nonnegativity
-    % nonLinear(h+1, :) = max(nonLinear(h+1, :), 0);%THIS IS WHAT I ADDED LAST NIGHT
-end
+options = odeset('NonNegative', 1:6);
+init = [120; 0; 11;120;0;11;0]; % Initial conditions: [G, X, I]
+[t,states] = ode45(@(t,states) simFunc(t, A, B, Dinterp, Kp, Ki, L, Gb, states), t, init, options);
 
 % Extract states
-G = nonLinear(:, 1);  % Blood glucose concentration
-X = nonLinear(:, 2);  % Insulin action
-I = nonLinear(:, 3);  % Plasma insulin concentration
+G = states(:, 1);
+X = states(:, 2);
+I = states(:, 3);
 
 figure;
 subplot(3, 1, 1);
@@ -215,40 +133,6 @@ sgtitle({'System State Responses', ...
     'FontSize', 12, 'FontWeight', 'bold')
 end
 end
-
-
-%%
-options = odeset('NonNegative', 1:6);
-init = [120; 0; 11;120;0;11;0]; % Initial conditions: [G, X, I]
-[t,states] = ode45(@(t,states) simFunc(t, A, B, Dinterp, K_p, K_i, L, Gb, states), t, init, options);
-
-
-figure;
-subplot(3, 1, 1);
-plot(t, states(:, 1),'color',blu,  'LineWidth', 1.5);
-xlabel('Time (min)');
-ylabel('G(t) (mg/dL)');
-title('Blood Glucose Concentration');
-grid on;
-
-subplot(3, 1, 2);
-plot(t, states(:, 2),'color',purp , 'LineWidth', 1.5);
-xlabel('Time (min)');
-ylabel('X(t)');
-title('Insulin Action Dynamics');
-grid on;
-
-subplot(3, 1, 3);
-plot(t, states(:, 3), 'g','color',grn,  'LineWidth', 1.5);
-xlabel('Time (min)');
-ylabel('I(t) (mU/L)');
-title('Plasma Insulin Concentration');
-grid on;
-
-sgtitle({'System State Responses', ...
-    sprintf('Disturbance Type: %s', disturbance), ...
-    sprintf('Control Mode: %s', mode)},...
-    'FontSize', 12, 'FontWeight', 'bold')
 %% Save images
 % filepath = "C:\Users\Will\OneDrive - Washington University in St. Louis\. Control Systems\Case Study 2\Figure Export";
 % exportgraphics(fh1, fullfile(filepath, 'part1 different vars.jpg'), 'resolution', 300);
