f6d3ad49633bcee0a8f3f7c808b6b943d87b0e84
Will <Will@Will-laptop.dhcp.wustl.edu>
2024-12-09 15:11:10 -0600

    WEEEEEEEEE

--- EfficacyAnalysis.m
+++ EfficacyAnalysis.m
@@ -12,6 +12,7 @@ blu = colors(1,:);
 org = colors(2,:);
 purp = colors(4,:);
 grn = colors(5,:);
+red = colors(7,:);
 
 close all
 figure
@@ -24,18 +25,41 @@ nBase = 0.1;       % Insulin clearance rate
 GbBase = 100;      % Baseline glucose
 IbBase = 10;       % Baseline plasma insulin
 
-t = 0:0.1:240;
 
-IC = [GbBase, 0, IbBase]; %should baseline insulin action be zero???
+dt = 0.05;
+tLim = 240;
+t = 0:dt:tLim;
 
-for j = 1:1 % Varies mode from auto to exercise. change to 2 later
-    if j ==1
-        mode = 'Auto';
-        u = zeros(length(t),1);
-    elseif j==2
-        mode = 'Exercise';
-        u = zeros(length(t),1);
-    end
+%% Deterine Observer/Controller
+%syms kp ki
+A = [-p1Base, -GbBase, 0;
+     0, -p2Base, p3Base;
+     0, 0, -nBase];
+B = [0;
+     0;
+     1];
+C = [1, 0, 0];
+
+
+
+%% Find gains based on mode
+for m = 1:2 % Varies mode from auto to exercise. change to 2 later
+if m == 1
+mode = 'Auto';
+controllerPoles = [-0.4, -0.3, -0.2, -0.1];
+observerPoles = [-2, -2.2, -2.4];
+elseif m==2
+mode = 'Exercise';
+controllerPoles = 0.75 .* [-0.4, -0.3, -0.2, -0.1];
+observerPoles = 0.75 .*[-2, -2.2, -2.4];
+end
+[Kp, Ki, L] = findGains(A, B, C, controllerPoles, observerPoles);
+
+%% Initialize Saved Results
+simLength = 30;
+meanErrors = zeros(simLength,4);
+maxErrors = zeros(simLength,4);
+
 for i = 1:4 % Sweeps through D values, change to 4 later
 if i == 1
 D = DGenerate('Monophasic',t,20,0,70,20,50, 0);
@@ -54,12 +78,6 @@ D = zeros(length(t),1);
 disturbance = 'No Disturbance';
 end
 
-%% Initialize Saved Results
-simLength = 30;
-meanErrors = zeros(simLength,1);
-maxErrors = zeros(simLength,1);
-
-simlen
 %% Reset 
 for k = 1:simLength % 
 Gout = zeros(length(t),30);
@@ -71,58 +89,107 @@ n = nBase   * (0.85 + (0.3 * rand));       % Insulin clearance rate
 Gb = GbBase;      % Baseline glucose
 Ib = IbBase;       % Baseline plasma insulin
 %% simulate ODE
-D_interp = @(t) interp1(0:0.1:240, D, t, 'linear', 'extrap');
-u_interp = @(t) interp1(0:0.1:240, u, t, 'linear', 'extrap');
+Dinterp = @(t) interp1(0:dt:tLim, D, t, 'linear', 'extrap');
 
-% dynamics = @(t, y) [
-%             -p1 * (y(1) - Gb) - y(2) * y(1) + D(round(t / 0.1) + 1);
-%             -p2 * y(2) + p3 * (y(3) - Ib);
-%             -n * y(3) + u(round(t / 0.1) + 1)];
-
-dynamics = @(t, y) [
-    -p1 * (y(1) - Gb) - y(2) * y(1) + D_interp(t);
-    -p2 * y(2) + p3 * (y(3) - Ib);
-    -n * y(3) + u_interp(t)];
-
-[t, y] = ode45(dynamics, t, IC);
+IC = [100; 0; 11; 100;0;10;0]; % Initial conditions: [G, X, I]
+[t,states] = ode45(@(t,states) simFuncAnalysis(t, A, B, Dinterp, Kp, Ki, L, Gb, states,p1,p2,p3,n), t, IC);
 
 % Extract states
-G = y(:, 1);  % Blood glucose concentration
-X = y(:, 2);  % Insulin action
-I = y(:, 3);  % Plasma insulin concentration
+G = states(:, 1);  % Blood glucose concentration
+X = states(:, 2);  % Insulin action
+I = states(:, 3);  % Plasma insulin concentration
 
-Gout(:,k) = G; 
+%Gout(:,k) = G; 
 %% Run analysis
 baseline = Gb;
 
-error = Gout - baseline;
-meanError = mean(error);
+error = G - baseline;
+meanError = mean(abs(error)); % mean error magnitude
 maxError = max(error);
 minError = min(error);
 
-meanErrors(k) = meanError;
-if (abs(maxError) > abs(minError))
-maxErrors(k) = maxError;
+meanErrors(k,i) = meanError;
+
+if (abs(maxError) >= abs(minError))
+maxErrors(k,i) = maxError;
 else
-maxErrors(k) = minError;
+maxErrors(k,i) = minError;
+end
+
+end % end sims
+
+end
+%% Figures
+%Boxplots
+if m == 1
+fh1 =figure;
+else
+fh3 = figure;
+end
+labels = {'Monophasic Nutrition', 'Biphasic Nutrition', 'Weightlifting Adrenaline', 'Sugarloaded Marathon'};
+
+boxplot(meanErrors, 'Labels', labels);
+ylabel('Mean Glucose Deviation (mg/dL)');
+grid on;
+
+% boxplot appearance change. not our code. taken from matlab doc
+h = findobj(gca, 'Tag', 'Box');
+for j = 1:length(h)
+    set(h(j), 'Color', blu, 'LineWidth', 2);
+end
+
+% Customize  median lines
+medianLines = findobj(gca, 'Tag', 'Median');
+for j = 1:length(medianLines)
+    set(medianLines(j), 'Color', red, 'LineWidth', 2); 
 end
 
 
+sgtitle(sprintf('Maximum Error Efficacy Analysis | %s Mode', mode),...
+    'FontSize', 12, 'FontWeight', 'bold')
+ylim([2,9]);
+% Barplots
+if m == 1
+fh2 =figure;
+else
+fh4 = figure;
 end
-
-boxplot(meanError)
-
-fakey = -20 + (40 * rand(1, 29));
-fakex = zeros(length(29),1);
-
-figure
-b = bar(fakey);
-% b.Barwidth = 2;
-% b.FaceColor = 'b';
-
+for i = 1:4
+subplot(2,2,i)
+b = bar(maxErrors(:,i));
 xlabel('Simulation #')
-ylabel('Glucose Deviation from 100mg/dL')
-title('Efficacy Analysis of Auto Mode')
-grid on
+ylabel('Deviation (mg/dL)')
+if i ==1
+title('Monophasic Nutriton')
+b.FaceColor = blu;
+elseif i ==2
+title('Biphasic Nutriton')
+b.FaceColor = org;
+
+elseif i==3
+title('Weightlifting Adrenaline')
+b.FaceColor = purp;
+
+elseif i==4
+title('Sugarloaded Marathon')
+b.FaceColor = grn;
 
 end
+end
+
+sgtitle({'Maximum Error Efficacy Analysis', ...
+    sprintf('Control Mode: %s', mode)},...
+    'FontSize', 12, 'FontWeight', 'bold')
+grid on
+end
+%% Save images
+% filepath = "C:\Users\Will\OneDrive - Washington University in St. Louis\. Control Systems\Case Study 2\Figure Export";
+% exportgraphics(fh1, fullfile(filepath, 'Analysis Mean Auto.jpg'), 'resolution', 300);
+% exportgraphics(fh2, fullfile(filepath, 'Analysis Max Auto.jpg'), 'resolution', 300);
+% exportgraphics(fh3, fullfile(filepath, 'Analysis Mean Exercise.jpg'), 'resolution', 300);
+% exportgraphics(fh4, fullfile(filepath, 'Analysis Max Exercise.jpg'), 'resolution', 300);
+% exportgraphics(fh5, fullfile(filepath, 'part2 delayed sine input u.jpg'), 'resolution', 300);
+% exportgraphics(fh6, fullfile(filepath, 'part2 delayed sine sim.jpg'), 'resolution', 300);
+% exportgraphics(fh7, fullfile(filepath, 'part2 zombies sim.jpg'), 'resolution', 300);
+% exportgraphics(fh8, fullfile(filepath, 'part3 networked sim no control.jpg'), 'resolution', 300);
+% exportgraphics(fh9, fullfile(filepath, 'part3 networked sim control.jpg'), 'resolution', 300);
